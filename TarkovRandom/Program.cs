bfd3684c4e1409f0d9b0e491669abb64c1110156
Lachie <you@example.com>
2021-06-23 14:29:22 +1000

    initial commit

---
+++ TarkovRandom/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace TarkovRandom
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine($"Armor: {(GetRandom(items.Armor))}");
+
+            Console.WriteLine($"Gun: {(GetRandom(items.Gun))}");
+
+            Console.WriteLine($"Rig: {(GetRandom(items.Rig))}");
+
+            Console.WriteLine($"Backpack: {(GetRandom(items.Backpack))}");
+
+            Console.WriteLine($"Gun Build: {(GetRandom(items.GunBuild))}");
+
+            Console.WriteLine($"Ammo: {(GetRandom(items.Ammo))}");
+
+            Console.WriteLine($"Map: {(GetRandom(items.Map))}");
+
+            Console.ReadKey();
+
+        }
+
+        public static string GetRandom(string[] itemArray)
+        {
+            Random random = new Random();
+                        
+            var randomItem = random.Next(0, itemArray.Length);
+
+            var itemPick = itemArray[randomItem];
+
+            return itemPick;
+        }
+    }
+}
---
+++ TarkovRandom/items.cs
@@ -0,0 +1,230 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace TarkovRandom
+{
+    class items
+    {
+        public static string[] Armor =
+        {
+            "3M",
+            "PACA",
+            "6B2",
+            "UNTAR",
+            "PRESS Vest",
+            "6B23-1",
+            "Kirara",
+            "USEC Trooper",
+            "6B13",
+            "6B23-2",
+            "Korund",
+            "Fort Redut-M",
+            "Killa",
+            "Gen4 HMK",
+            "Ghzel",
+            "Defender",
+            "Gen4 Assault",
+            "Gen4 Full",
+            "Fort Redut-T5",
+            "Slick",
+            "Zhuk-6a",
+            "Zabralo"
+        };
+
+        public static string[] Gun =
+        {
+            "ADAR",
+            "AK-101",
+            "AK-102",
+            "AK-103",
+            "AK-104",
+            "AK-105",
+            "AK-74",
+            "AK-74M",
+            "AK-74N",
+            "AKM",
+            "AKMN",
+            "AKMSN",
+            "AKS-74",
+            "AKS-74N",
+            "AKS-74U",
+            "AKS-74UB",
+            "AKS-74UN",
+            "AS VAL",
+            "ASH-12",
+            "DT MDR .308",
+            "HK 416A5",
+            "RFB",
+            "M4A1",
+            "MCX",
+            "SA-58",
+            "TX-15",
+            "VPO-209",
+            "VPO-136",
+            "OP-SKS",
+            "SKS",
+            "Hunter",
+            "RPK",
+            "MP5",
+            "MP5K",
+            "MP7A1",
+            "MP7A2",
+            "MP9",
+            "MP9-N",
+            "MPX",
+            "P90",
+            "PP-19",
+            "PP-9 Klin",
+            "PP-91 Kedr",
+            "PP-91-01 Kedr-B",
+            "PPSH",
+            "Saiga-9",
+            "STM-9",
+            "UMP-45",
+            "Vector .45",
+            "Vector 9mm",
+            "590A1",
+            "M870",
+            "MP-133",
+            "MP-153",
+            "Sagia 12",
+            "TOZ-106",
+            "KS-23M",
+            "M1A",
+            "Mk-18",
+            "RSASS",
+            "SR-25",
+            "SVDS",
+            "VSS",
+            "DVL",
+            "M700",
+            "Mosin",
+            "Mosin Infantry",
+            "SV-98",
+            "T-5000",
+            "VPO-215",
+            "GL40"
+        };
+
+        public static string[] Helmet =
+        {
+            "Tac-KEK Fast MT",
+            "Tanker Helmet",
+            "Kolpak",
+            "SHPM",
+            "UNTAR",
+            "Ratnik",
+            "LZSh",
+            "Peen",
+            "Kiver",
+            "Ronin",
+            "Sfera-S",
+            "TC-2001",
+            "TC-2002",
+            "TC-800",
+            "ACHHC IIIA",
+            "ZSh-1-2M",
+            "ULACH",
+            "Bastion",
+            "Fast MT",
+            "Airframe",
+            "EXFIL",
+            "Caiman",
+            "LZHZ-2DTM",
+            "Maska",
+            "Altyn",
+            "Rys-T",
+            "Vulkan",
+            "Cowboy Hat",
+            "Ushanka",
+            "Panama",
+            "Door Kicker",
+            "Cap"
+        };
+
+        public static string[] Rig =
+        {
+            "Scav Vest",
+            "Security Vest",
+            "IDEA Vest",
+            "Bank Robber",
+            "SOE Micro",
+            "Wartech TV-109",
+            "CSA Chest Rig",
+            "Scout Sniper",
+            "Tarzan",
+            "D3CRX",
+            "Triton",
+            "Commando",
+            "Thunderbolt",
+            "Gear Craft GC-BSS MK1",
+            "Umka",
+            "LBT-1961A",
+            "Blackrock",
+            "MK3",
+            "Alpha",
+            "Azimut",
+            "MPPV",
+            "Belt-A + Belt-B"
+        };
+
+        public static string[] Backpack =
+        {
+            "Couch Bag",
+            "Blackjack",
+            "F4",
+            "Attack2",
+            "Pilgrim",
+            "Paratus",
+            "Gunslinger",
+            "Mechanism",
+            "Tri-Zip",
+            "Beta-2",
+            "F5",
+            "Baguette",
+            "Berkut",
+            "Day Pack",
+            "Scav Bag",
+            "MBSS",
+            "Sanitar Bag",
+            "Duffle",
+            "LK 3F",
+            "T-Bag",
+            "Army Bag",
+            "Sling"
+        };
+
+        public static string[] GunBuild =
+        {
+            "Level 1 Trader",
+            "Level 2 Trader",
+            "Level 3 Trader",
+            "Level 4 Trader"
+        };
+
+        public static string[] Ammo =
+        {
+            "Level 1 Trader",
+            "Level 2 Trader",
+            "Level 3 Trader",
+            "Level 4 Trader"
+        };
+
+        public static string[] Map =
+        {
+            "Factory",
+            "Customs",
+            "Woods",
+            "Shorline",
+            "Reserve",
+            "Interchange",
+            "Labs"
+        };
+
+        public static string[] Joker =
+        {
+            "blah"
+        };
+    }
+}
